f3e3d0beeec82b02d30f6448824495d4378a76bf
Sakshi Gautam <sakshigautam90111@gmail.com>
2025-12-04 10:29:06 +0530

    this is javascript tutorial

---
+++ tut2(variable).js
@@ -0,0 +1,7 @@
+console.log("This repl contains code for the ultimate Javascript course video 2 ")
+let a =67 // a contains 67
+console.log(a)
+a = "harry"
+console.log(a)
+//let 8harry = 7 // not allowed this throw an error
+//let var = 7
---
+++ tut3(var_let and _const).js
@@ -0,0 +1,18 @@
+console.log("Javascript tutorial 3: var, let and const")
+//  var a = 45;
+//  let b = 4
+ let b = "Harry";
+ const author = "Harry"
+ // author = 5 // throws Error because constant cannot be chnaged
+ const harry = 0;
+ b = 4
+ let c = null
+ let d = undefined
+ {
+  // var b = 'this'
+  // console.log(b)
+
+  let b = 'this'
+  console.log(b)
+ }
+ console.log(b)
