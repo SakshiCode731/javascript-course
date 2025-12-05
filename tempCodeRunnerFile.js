a687e38cca841d682a17da57338299e3559c284e
Sakshi Gautam <sakshigautam90111@gmail.com>
2025-12-05 22:41:09 +0530

    This is Expression Code

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,13 @@
-a1 = "Harry"
+let a = 10;
+// let b = 4;
+// console.log("a + b =", a + b)
+// console.log("a - b =", a - b)
+// console.log("a/b =", a / b)
+// console.log("a ** b = ", a ** b)
+// console.log("a % b =", a % b)
+// console.log("++a = ", ++a)
+// console.log("++a = ", a++)
+// console.log("--a = ", --a)
+// console.log("a-- = ", a--)
+// console.log("a =", a)
+// console.log("a-- =", a--)
---
+++ tut6(Operation_&_Expression).js
@@ -0,0 +1,45 @@
+// this is single line comment
+/* this is muitiple line comment*/
+console.log("operators in js")
+//Arithmetic Operators
+let a = 10;
+let b = 4;
+console.log("a + b =", a + b)
+console.log("a - b =", a - b)
+console.log("a/b =", a / b)
+console.log("a ** b = ", a ** b)
+console.log("a % b =", a % b)
+console.log("++a = ", ++a)
+console.log("++a = ", a++)
+console.log("--a = ", --a)
+console.log("a-- = ", a--)
+console.log("a =", a)
+console.log("a-- =", a--)
+
+// Assignment Operator
+let assignment = 1;
+// assignment += 5 //same as assignment = assignment + 5
+// console.log(assignment)
+console.log("a is now =", a)
+assignment -= 5
+console.log("a is now =", a)
+assignment *= 5
+console.log("a is now", a)
+assignment /= 5
+console.log("a is now = ", a)
+
+// Comparision Operators
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1 == comp2 is", comp1 == comp2)
+console.log("comp1 != comp2 is", comp1 != comp2)
+console.log("comp1 === comp2 is", comp1 === comp2)
+console.log("comp1 !== comp2 is", comp1 !==comp2)
+
+//Logical Operator
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false)
+console.log(!true)
