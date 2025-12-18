63db48d6ac297b77412a4de4791471c735de984f
Sakshi Gautam <sakshigautam90111@gmail.com>
2025-12-18 12:09:55 +0530

    This is practice set

---
+++ tut46_practice_set.js
@@ -0,0 +1,20 @@
+// // Q3
+// document.getElementById("google").addEventListener('click', function (e) {
+//   window.location = "https://www.google.com";
+//   window.focus();
+// })
+
+// const fetchContent = async (url) => {
+//   con = await fetch(url);
+//   let a = await con.json();
+//   return a;
+// }
+
+// setInterval(async function (){
+//   let url = 'https://jsonplaceholder.typicode.com/todos/1'
+//       console.log(await fetchContent(url));
+// },3000)
+
+setInterval(async function (){
+  document.querySelector("#bulb").classList.toggle("bulb");
+},200)
