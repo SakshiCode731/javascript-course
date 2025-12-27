d05002f298d8758555eab7d05f6a90645ffc2324
Sakshi Gautam <sakshigautam90111@gmail.com>
2025-12-27 10:19:58 +0530

    this is cookis file

--- js_file/tut64_cookies.js
+++ js_file/tut64_cookies.js
@@ -1,8 +1,8 @@
 console.log(document.cookie)
 document.cookie = "name=sakshi12334"
 document.cookie = "name2=sakshi12334"
-document.cookie = "name=sakshi"
+document.cookie = "name=sakshi"  //it is called setcall
 let key = prompt("enter your key")
 let value = prompt("enter your value")
-document.cookie = `${(key)} = ${(value)}`
+document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}` //it is called setcall
 console.log(document.cookie)
