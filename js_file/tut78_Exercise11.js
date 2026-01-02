577501a25f25f935d7bc472f753c82d3e89d5246
Sakshi Gautam <sakshigautam90111@gmail.com>
2026-01-02 17:38:15 +0530

    This is exercise code

---
+++ js_file/tut78_Exercise11.js
@@ -0,0 +1,55 @@
+class Complex {
+  constructor(real, imaginary) {
+    this.real = real
+    this.imaginary = imaginary
+  }
+  add(num) {
+    this.real = this.real + num.real
+    this.imaginary = this.imaginary + num.imaginary
+
+  }
+  get real() {
+    return this._real
+  }
+
+  get imaginary(){
+    return this._imaginary
+  }
+
+  set imaginary(newImaginary){
+    this._imaginary = newImaginary
+  }
+
+  set real(newReal){
+    this._real = newReal
+  }
+}
+
+let a = new Complex(2, 4)
+console.log(a.real, a.imaginary)
+a.real = 10
+a.imaginary = 10
+let b = new Complex(6, 2)
+a.add(b)
+console.log(`${a.real}+${a.imaginary}i`)
+
+// Question 3 and 4
+// class Human {
+//   constructor(name, favfood) {
+//     this.name = name
+//     this.favfood = favfood
+//   }
+//   walk() {
+//     console.log(this.name + "Human is Walking")
+//   }
+// }
+
+// class Students extends Human {
+//   walk() {
+//     console.log(this.name + ": Student is walking")
+//   }
+// }
+
+// let o = new Students("Sakshi", "Gobhi")
+// o.walk()
+// console.log(o instanceof Human)
