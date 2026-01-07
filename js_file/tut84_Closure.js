555917465b52f1f44469d3c7f1a90c2c9895a403
Sakshi Gautam <sakshigautam90111@gmail.com>
2026-01-07 22:34:07 +0530

    This is closure File

--- js_file/tut84_Closure.js
+++ js_file/tut84_Closure.js
@@ -1,14 +1,49 @@
-message = "Good Global"
-function hello(){
-  let message = "Good Morning"
-  {
-    let message = "Good Afternoon" // only vaalid in this block after that upper good morning print
-    console.log("Hello1: " + message)
+// closure is a function along with lexical environment
+
+// message = "Good Global"
+// function hello(){
+//   let message = "Good Morning"
+//   {
+//     let message = "Good Afternoon" // only vaalid in this block after that upper good morning print
+//     console.log("Hello1: " + message)
+//   }
+//   let c = function hello2(){
+//     console.log("I am c " + message)
+//   }
+//   return c
+// }
+// c = hello()
+// c()
+
+function init() {
+  var name = "Mozilla"; // name is a local variable created by init
+  function displayName() {
+    // displayName() is the inner function, that forms a closure
+    console.log(name); // use variable declared in the parent function
   }
-  let c = function hello2(){
-    console.log("I am c " + message)
-  }
-  return c
+  name = "Sakshi" // return reference of function
+  return displayName;
 }
-c = hello()
-c()
+
+function returnfunc(){
+const x = ()=>{
+  let a = 1
+  console.log(a)
+  const y = ()=>{
+    // let a = 2
+    console.log(a)
+    const z = ()=>{
+      // let a = 3
+      console.log(a)
+    }
+    z()
+  }
+  a = 999
+  y()
+}
+x()
+}
+let a = returnfunc()
+a()
+// let c = init();
+// c()
