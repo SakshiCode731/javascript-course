361f4c587fcc143ab12032787923bff031f26e75
Sakshi Gautam <sakshigautam90111@gmail.com>
2026-01-15 22:09:02 +0530

    This Is Module File

---
+++ node/module2.js
@@ -0,0 +1,14 @@
+// ES6 Modules
+
+export const hello = ()=>{
+  console.log("Hello Sakshi")
+}
+
+export const ahello = (name)=>{
+  console.log("Hello" + name)
+}
+
+const sakshi = ()=>{
+   console.log("Hello" + " Sakshi")
+}
+export default sakshi ;
---
+++ node/modules.js
@@ -0,0 +1,9 @@
+// const hello = require("./module1")
+// const {hello, ahello} = require("./module1") // common js
+
+import sakshi, {hello, ahello} from "./module2.js";
+hello()
+ahello(" Sakshi")
+ahello(" Sreya")
+ahello(" Priya")
+sakshi()
