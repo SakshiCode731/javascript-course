b23c217525178ccce4d448f5e2ab9b90a23cf47c
Sakshi Gautam <sakshigautam90111@gmail.com>
2026-01-18 19:55:03 +0530

    This is Express file

--- node/express.js
+++ node/express.js
@@ -1,61 +1,33 @@
-// const http = require('http')
-
-// // Use environment variable PORT if set, otherwise default to 3000
-// const port = process.env.PORT || 5000; // changed 3000 to 5000
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' })
- 
-
-//   res.end('SERVER OK')
-// })
-
-// server.listen(port, () => {
-//   console.log(`HTTP server running on http://localhost:${port}`)
-// })
-
-// const express = require('express')
-// const app = express()
-// const port = 5001
-
-// app.get('/', (req, res) => {
-//   console.log(req)
-//   res.send('Hello World')
-// })
-
-// app.get('/about', (req, res) => {
-//   res.send('about')
-// })
-
-// app.get('/contact', (req, res) => {
-//   res.send('contact')
-// })
-
-// app.get('/map', (req, res) => {
-//   res.send('map')
-// })
-
-// app.use(express.static('public')) // ⬅ move BELOW routes
-
-// app.listen(port, () => {
-//   console.log(`Server running at http://localhost:${port}`)
-// })
-
-
-
-const express = require('express');
-const app = express();
-const port = 5001;
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 5001
 
 app.get('/', (req, res) => {
-    // Get the 'name' query parameter from URL
-    const name = req.query.name || 'Guest'; // default to 'Guest' if no name
-    console.log(`Name from Query: ${name}`);
+  console.log(req.query)
+  res.send('Hello World')
+})
 
-    // Send response to browser
-    res.send(`<h1>Hello, ${name}!</h1>`);
-});
+app.get('/about', (req, res) => {
+    
+  res.sendFile(path.join(__dirname, './index.html'));
+//   res.send('about')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('contact')
+})
+
+app.get('/map', (req, res) => {
+  res.send('map')
+})
+
+app.use(express.static('public')) // ⬅ move BELOW routes
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
+
+
+
+
