b355c2203cc99d025096c5e9afcf9ca79c22328c
Sakshi Gautam <sakshigautam90111@gmail.com>
2026-01-19 20:41:20 +0530

    This is Regular Expression Updated COde

--- node/Regular_Expression.js
+++ node/Regular_Expression.js
@@ -1,4 +1,4 @@
-const regex = /very/g
-const text = "Sakshi is a very very nice very awesome nice girl"
+const regex = /(Sakshi){2}/gi
+const text = "Sakshisakshi is a very very nice very awesome nice girl"
 // console.log(text.replace("very", "VERY"))
 console.log(text.replace(regex, "VERY"))
