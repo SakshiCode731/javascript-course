9a904478df0d5c54400cb0419133b03fb0cacfec
Sakshi Gautam <sakshigautam90111@gmail.com>
2026-02-24 21:26:55 +0530

    there is adding some code

--- js_file/tut25_console_method.js
+++ js_file/tut25_console_method.js
@@ -12,9 +12,9 @@ for(let i =0;i<5;i++){
 console.timeEnd("forLoop")
 
 console.time("whileLoop")
-let i = 0;
-while(i<5){
+let a = 0;
+while(a<5){
   console.log(233)
-  i++;
+  a++;
 }
 console.timeEnd("whileLoop")
