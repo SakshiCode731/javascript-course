67f7f9c1f7642ac157ab5109767ec4582575a875
Sakshi Gautam <sakshigautam90111@gmail.com>
2026-02-28 21:14:51 +0530

    ther is adding some code

--- node js/server.js
+++ node js/server.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
